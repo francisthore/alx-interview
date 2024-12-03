be12be0b0c6d8a986a8915ba3820bc50dba2e530
Francis Thore <thorefrancis@gmail.com>
2024-12-04 00:06:05 +0200

    island param

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,14 @@
+def island_perimeter(grid):
+    """Calculate perimeter."""
+    width, height = len(grid[0]), len(grid)
+    edges, size = 0, 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    edges += 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    edges += 1
+    return size * 4 - edges * 2
