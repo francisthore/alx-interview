f39a4aa76d567ea8c343742bb5797253d4eceea8
Francis Thore <thorefrancis@gmail.com>
2024-12-12 22:42:15 +0200

    prime game code upload

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+  Determines the winner of a game based on the strategic
+  removal of prime numbers and their multiples from a set
+  of consecutive integers.
+"""
+
+
+def is_winner(x, nums):
+    maria_wins = 0
+    ben_wins = 0
+
+    for num in nums:
+        rounds = list(range(1, num + 1))
+        primes = get_primes(1, num)
+
+        if not primes:
+            ben_wins += 1
+            continue
+
+        maria_turn = True
+
+        while True:
+            if not primes:
+                if maria_turn:
+                    ben_wins += 1
+                else:
+                    maria_wins += 1
+                break
+
+            prime = primes.pop(0)
+            rounds.remove(prime)
+            rounds = [n for n in rounds if n % prime != 0]
+            maria_turn = not maria_turn
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    if ben_wins > maria_wins:
+        return "Ben"
+    return None
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def get_primes(start, end):
+    return [n for n in range(start, end + 1) if is_prime(n)]
